5bb260d0c65b200f7d977deb54449ccbe78c0a4e
sabikunnaharsafa11 <sabiunnaharsafa92@gmail.com>
2021-09-02 13:59:45 +0600

    value

--- js/script.js
+++ js/script.js
@@ -1,57 +1,56 @@
 
 const seachBook = () => {
-    const input = document.getElementById('search-input');
-    const inputValue = input.value;
+    const searchInput = document.getElementById('search-input');
+    const searchText = searchInput.value;
+    searchInput.value = '';
 
 
-    BookApiLoad(inputValue)
-    input.value = '';
-
-
-}
-
-const BookApiLoad = data => {
-    const url = `https://openlibrary.org/search.json?q=${data}`
-    fetch(url)
-        .then(res => res.json())
-        .then(data => displayData(data.docs))
-
+    const url = `https://openlibrary.org/search.json?q=${searchText}`
+        fetch(url)
+            .then(res => res.json())
+            .then(data => displayData(data.docs))   
 
 }
 
-const displayData = docsData => {
+const displayData = data => {
 
-
-
-    console.log(docsData)
+    // console.log(docsData)
     const item = document.getElementById('item')
-
-
-    docsData.forEach(ele => {
+    item.textContent = '';
+    data.slice(0, 12).forEach(e => {
         const div = document.createElement('div');
-
         div.classList.add('col');
-
-
-        div.innerHTML = `
+       div.innerHTML = `
         <div class="card h-100">
-        <img id="img" src="https://covers.openlibrary.org/b/id/${ele.cover_i}-M.jpg" class="card-img-top" alt="...">
+        <img id="img" src="https://covers.openlibrary.org/b/id/${e.cover_i}-M.jpg" class="card-img-top" alt="...">
         <div class="card-body">
-            <h5 class="card-title">Book Name : ${ele.title}</h5>
-            <p class="card-text">Author Name : ${ele.author_name[0]}</p>
-            <small class="text-muted">Publisher Name : ${ele.publisher}</small>
+            <h5 class="card-title">Book Name : ${e.title}</h5>
+            <p class="card-text">Author Name : ${e.author_name}</p>
+            <small class="text-muted">First Publish Year : ${e.first_publish_year}</small>
         </div>
       
        `
-
         item.appendChild(div);
+        // Search item Numbers
+      displayAllItem  (data.length,'block');
 
+    });
 
+    if (data.length === 0) {
+        const notFound = document.getElementById('not-found');
+        notFound.innerText = `Opps! No Result Found!!`
 
-    })
+    } else {
+        const notFound = document.getElementById('not-found');
+        notFound.style.display = 'none';
 
-
-
-
+    }
 
 }
+
+// search Items number
+const displayAllItem = (itemLength) =>{
+    const displayNumbers = document.getElementById('total-search-found');
+    displayNumbers.innerText = `Search Item Number : ( ${itemLength} )`;
+    displayNumbers.style.display = 'block';
+};
